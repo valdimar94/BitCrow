8f2c996498757d23bf6a6d815b94fdf8a7775e00
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-10 11:23:52 +0000

    fixed a bug where git install would not clean up after it's self

--- git/install.sh
+++ git/install.sh
@@ -26,7 +26,7 @@ if [ $installgitlfs == "y" ]
     tar -zxvf git-lfs-linux-amd64-1.4.4.tar.gz
     rm git-lfs-linux-amd64-1.4.4.tar.gz
     chmod +x git-lfs-1.4.4/install.sh
-    cd git-lfs-1.4.4 && sudo ./install.sh
+    (cd git-lfs-1.4.4 && sudo ./install.sh)
     rm -rf git-lfs-1.4.4
     sudo git lfs install
 fi
