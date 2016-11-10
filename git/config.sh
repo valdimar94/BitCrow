4d9e36e58e8ae9d7fe8d06953da34119a8b4f61d
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-10 11:13:39 +0000

    added git lfs in git installer

--- git/config.sh
+++ git/config.sh
@@ -10,4 +10,8 @@ echo "What is your name?"
 read name
 echo "$name" > $(dirname $0)/name.config
 
+echo "Would you also like to install git lfs (Large File Storage)(y/n)?"
+read installgitlfs
+echo "$installgitlfs" > $(dirname $0)/installgitlfs.config
+
 exit 0
--- git/install.sh
+++ git/install.sh
@@ -15,7 +15,20 @@ if [ ! -f ~/.ssh/id_rsa ]
     ssh-keygen -t rsa -b 4096 -C "$email"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
+    sensible-browser https://github.com/settings/keys
     gedit ~/.ssh/id_rsa.pub
+fi
+
+installgitlfs=$(cat $(dirname $0)/installgitlfs.config)
+if [ $installgitlfs == "y" ]
+  then
+    wget https://github.com/github/git-lfs/releases/download/v1.4.4/git-lfs-linux-amd64-1.4.4.tar.gz
+    tar -zxvf git-lfs-linux-amd64-1.4.4.tar.gz
+    rm git-lfs-linux-amd64-1.4.4.tar.gz
+    chmod +x git-lfs-1.4.4/install.sh
+    cd git-lfs-1.4.4 && sudo ./install.sh
+    rm -rf git-lfs-1.4.4
+    sudo git lfs install
 fi
 
 exit 0
