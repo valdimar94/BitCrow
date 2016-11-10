f3cd2624ce4d3c0b764672588f55de32a4a98471
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-10 11:24:24 +0000

    added chrome to the setup process

---
+++ chrome/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+rm google-chrome*.deb
+
+exit 0
