a57c10ca38e51081dcb24d9cc77badecdceeabb9
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-10 19:03:51 +0000

    added options to remove stock apps in the settings script

--- settings/settings.sh
+++ settings/settings.sh
@@ -2,6 +2,27 @@
 
 set -e
 
+echo "Would you like remove ubuntu amazon(y/n)?"
+read answer
+if [ "$answer" == "y" ]
+  then
+    sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+fi
+
+echo "Would you like remove firefox(y/n)?"
+read answer
+if [ "$answer" == "y" ]
+  then
+    sudo apt-get -y remove firefox
+fi
+
+echo "Would you like remove libreoffice(y/n)?"
+read answer
+if [ "$answer" == "y" ]
+  then
+    sudo apt-get -y remove libreoffice*
+fi
+
 unity-control-center display
 unity-control-center power
 unity-control-center screen
